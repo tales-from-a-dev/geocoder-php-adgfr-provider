cca4b3e708c565c9f0304a8b79e09de541b7d239
Romain Monteil <monteil.romain@gmail.com>
2024-01-31 09:50:40 +0100

    :white_check_mark: Add tests

--- src/Adgfr.php
+++ src/Adgfr.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Geocoder\Provider\Adgfr;
 
 use Geocoder\Collection;
-use Geocoder\Exception\InvalidArgument;
 use Geocoder\Exception\InvalidServerResponse;
 use Geocoder\Exception\UnsupportedOperation;
 use Geocoder\Http\Provider\AbstractHttpProvider;
@@ -24,73 +23,42 @@ final class Adgfr extends AbstractHttpProvider
 
     public function geocodeQuery(GeocodeQuery $query): Collection
     {
-        $address = $query->getText();
-
         // This API doesn't handle IPs
-        if (filter_var($address, \FILTER_VALIDATE_IP)) {
+        if (filter_var($query->getText(), \FILTER_VALIDATE_IP)) {
             throw new UnsupportedOperation('The Adgfr provider does not support IP addresses.');
         }
 
-        $url = sprintf('%s/search/?q=%s', self::API_URL, urldecode($address));
+        $url = sprintf(
+            '%s/search/?%s',
+            self::API_URL,
+            http_build_query([
+                'q' => $query->getText(),
+                'limit' => $query->getLimit(),
+                'lat' => $query->getData('lat'),
+                'lon' => $query->getData('lon'),
+                'postcode' => $query->getData('postcode'),
+                'citycode' => $query->getData('citycode'),
+                'type' => Type::tryFrom($query->getData('type', ''))?->value,
+            ], '', '&', \PHP_QUERY_RFC3986)
+        );
 
-        if ($limit = $query->getLimit()) {
-            $url .= sprintf('&limit=%d', $limit);
-        }
-
-        if ($autocomplete = $query->getData('autocomplete')) {
-            $url .= sprintf('&autocomplete=%d', $autocomplete);
-        }
-
-        if (
-            ($lat = $query->getData('lat'))
-            && ($lon = $query->getData('lon'))
-        ) {
-            $url .= sprintf('&lat=%F&lon=%F', $lat, $lon);
-        }
-
-        if ($type = $query->getData('type')) {
-            if (\is_string($type)) {
-                $type = Type::from($type);
-            }
-
-            if (!$type instanceof Type) {
-                throw new InvalidArgument(sprintf('"type" must be a valid "%s" enum, "%s" given.', Type::class, get_debug_type($type)));
-            }
-
-            $url .= sprintf('&type=%s', $type->value);
-        }
-
-        if ($postcode = $query->getData('postcode')) {
-            $url .= sprintf('&postcode=%s', $postcode);
-        }
-
-        if ($citycode = $query->getData('citycode')) {
-            $url .= sprintf('&citycode=%s', $citycode);
-        }
-
-        $content = $this->executeQuery($url);
-
-        $json = json_decode($content, false, 512, JSON_THROW_ON_ERROR);
-
-        if (!is_object($json)) {
-            throw InvalidServerResponse::create($url);
-        }
-
-        if (empty($json)) {
-            return new AddressCollection([]);
-        }
-
-        $results = [];
-        foreach ($json->features as $feature) {
-            $results[] = $this->jsonResultToLocation($feature, false);
-        }
-
-        return new AddressCollection($results);
+        return $this->executeQuery($url);
     }
 
     public function reverseQuery(ReverseQuery $query): Collection
     {
-        return new AddressCollection([]);
+        $coordinates = $query->getCoordinates();
+
+        $url = sprintf(
+            '%s/reverse/?%s',
+            self::API_URL,
+            http_build_query([
+                'lat' => $coordinates->getLatitude(),
+                'lon' => $coordinates->getLongitude(),
+            ], '', '&', \PHP_QUERY_RFC3986)
+        );
+
+        return $this->executeQuery($url);
     }
 
     public function getName(): string
@@ -98,22 +66,35 @@ final class Adgfr extends AbstractHttpProvider
         return self::PROVIDER_NAME;
     }
 
-    private function executeQuery(string $url): string
+    private function executeQuery(string $url): AddressCollection
     {
-        return $this->getParsedResponse($this->getRequest($url));
+        $content = $this->getParsedResponse($this->getRequest($url));
+
+        $json = json_decode($content, false, 512, \JSON_THROW_ON_ERROR);
+
+        if (!$json instanceof \stdClass) {
+            throw InvalidServerResponse::create($url);
+        }
+
+        $results = [];
+        foreach ($json->features as $feature) {
+            $results[] = $this->jsonResultToLocation($feature);
+        }
+
+        return new AddressCollection($results);
     }
 
-    private function jsonResultToLocation(\stdClass $feature, bool $reverse): Location
+    private function jsonResultToLocation(\stdClass $feature): Location
     {
-        [$latitude, $longitude] = $feature->geometry->coordinates;
+        [$longitude, $latitude] = $feature->geometry->coordinates;
 
         $builder = new AddressBuilder($this->getName());
         $builder
             ->setCoordinates($latitude, $longitude)
             ->setStreetNumber($feature->properties->housenumber ?? null)
             ->setStreetName($feature->properties->street ?? null)
-            ->setPostalCode($feature->properties->postcode)
-            ->setLocality($feature->properties->city)
+            ->setPostalCode($feature->properties->postcode ?? null)
+            ->setLocality($feature->properties->city ?? null)
             ->setCountry('France')
             ->setCountryCode('FR')
         ;
--- tests/AdgfrTest.php
+++ tests/AdgfrTest.php
@@ -10,8 +10,10 @@ use Geocoder\IntegrationTest\BaseTestCase;
 use Geocoder\Model\Address;
 use Geocoder\Model\AddressCollection;
 use Geocoder\Provider\Adgfr\Adgfr;
+use Geocoder\Provider\Adgfr\Enum\Type;
 use Geocoder\Provider\Adgfr\Model\AdgfrAddress;
 use Geocoder\Query\GeocodeQuery;
+use Geocoder\Query\ReverseQuery;
 
 final class AdgfrTest extends BaseTestCase
 {
@@ -35,11 +37,91 @@ final class AdgfrTest extends BaseTestCase
         $this->assertInstanceOf(AddressCollection::class, $results);
         $this->assertCount(5, $results);
 
+        /** @var AdgfrAddress $address */
+        $address = $results->first();
+        $this->assertInstanceOf(Address::class, $address);
+        $this->assertEqualsWithDelta(48.850699, $address->getCoordinates()->getLatitude(), 0.00001);
+        $this->assertEqualsWithDelta(2.308628, $address->getCoordinates()->getLongitude(), 0.00001);
+        $this->assertSame('20', $address->getStreetNumber());
+        $this->assertSame('Avenue de Ségur', $address->getStreetName());
+        $this->assertSame('75007', $address->getPostalCode());
+        $this->assertSame('Paris', $address->getLocality());
+        $this->assertSame('FR', $address->getCountry()->getCode());
+        $this->assertSame('75107_8909_00020', $address->getId());
+        $this->assertSame(Type::HouseNumber, $address->getType());
+        $this->assertSame('75107', $address->getCityCode());
+        $this->assertSame('Paris 7e Arrondissement', $address->getDistrict());
+    }
+
+    public function testGeocodeHouseNumberTypeQuery(): void
+    {
+        $provider = new Adgfr($this->getHttpClient());
+        $results = $provider->geocodeQuery(
+            GeocodeQuery::create('20 avenue Kléber, Paris')->withData('type', Type::HouseNumber->value)
+        );
+
+        $this->assertInstanceOf(AddressCollection::class, $results);
+
+        /** @var AdgfrAddress $address */
+        $address = $results->first();
+        $this->assertInstanceOf(Address::class, $address);
+        $this->assertSame('20', $address->getStreetNumber());
+        $this->assertSame('Avenue Kléber', $address->getStreetName());
+        $this->assertSame('75016', $address->getPostalCode());
+        $this->assertSame('Paris', $address->getLocality());
+    }
+
+    public function testGeocodeStreetTypeQuery(): void
+    {
+        $provider = new Adgfr($this->getHttpClient());
+        $results = $provider->geocodeQuery(
+            GeocodeQuery::create('20 avenue Kléber, Paris')->withData('type', Type::Street->value)
+        );
+
+        $this->assertInstanceOf(AddressCollection::class, $results);
+
+        /** @var AdgfrAddress $address */
+        $address = $results->first();
+        $this->assertInstanceOf(Address::class, $address);
+        $this->assertNull($address->getStreetNumber());
+        $this->assertSame('Avenue Kléber', $address->getStreetName());
+        $this->assertSame('75016', $address->getPostalCode());
+        $this->assertSame('Paris', $address->getLocality());
+    }
+
+    public function testGeocodeLocalityQuery(): void
+    {
+        $provider = new Adgfr($this->getHttpClient());
+        $results = $provider->geocodeQuery(
+            GeocodeQuery::create('20 avenue Kléber, Paris')->withData('type', Type::Locality->value)
+        );
+
+        $this->assertInstanceOf(AddressCollection::class, $results);
+
+        /** @var AdgfrAddress $address */
+        $address = $results->first();
+        $this->assertInstanceOf(Address::class, $address);
+        $this->assertNull($address->getStreetNumber());
+        $this->assertNull($address->getStreetName());
+        $this->assertEqualsWithDelta(48.994028, $address->getCoordinates()->getLatitude(), 0.00001);
+        $this->assertEqualsWithDelta(2.509919, $address->getCoordinates()->getLongitude(), 0.00001);
+        $this->assertSame('95700', $address->getPostalCode());
+        $this->assertSame('Roissy-en-France', $address->getLocality());
+    }
+
+    public function testReverseWithCoordinates(): void
+    {
+        $provider = new Adgfr($this->getHttpClient());
+        $results = $provider->reverseQuery(ReverseQuery::fromCoordinates(48.850699, 2.308628));
+
+        $this->assertInstanceOf(AddressCollection::class, $results);
+        $this->assertCount(5, $results);
+
         /** @var AdgfrAddress $result */
         $address = $results->first();
         $this->assertInstanceOf(Address::class, $address);
-        $this->assertEqualsWithDelta(2.308628, $address->getCoordinates()->getLatitude(), 0.00001);
-        $this->assertEqualsWithDelta(48.850699, $address->getCoordinates()->getLongitude(), 0.00001);
+        $this->assertEqualsWithDelta(48.850699, $address->getCoordinates()->getLatitude(), 0.00001);
+        $this->assertEqualsWithDelta(2.308628, $address->getCoordinates()->getLongitude(), 0.00001);
         $this->assertSame('20', $address->getStreetNumber());
         $this->assertSame('Avenue de Ségur', $address->getStreetName());
         $this->assertSame('75007', $address->getPostalCode());
